d13749549a899f9bba4f36db51b543e4e4511a26
MRafliAditya <rafli14aditya@gmail.com>
2023-09-29 19:58:57 +0700

    naming

--- resources/views/template/layout.blade.php
+++ resources/views/template/layout.blade.php
@@ -15,14 +15,16 @@
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
-
-
+    
+    
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.css" rel="stylesheet">
-
+    
     <!-- Link IMG -->
     <link rel="shortcut icon" href="https://www.trafoindonesia.com/favicon.ico" type="image/x-icon">
     <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+    
+    <link href="/css/style.css" rel="stylesheet" type="text/css">
     
 </head>
 
