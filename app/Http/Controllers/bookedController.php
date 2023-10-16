6d870a3089c1ee20e52c5f472ffeaf8494e93280
1nverse <wan.aushaf@gmail.com>
2023-10-16 16:24:39 +0700

    biar bisa merge

--- app/Http/Controllers/bookedController.php
+++ app/Http/Controllers/bookedController.php
@@ -18,20 +18,16 @@ class bookedController extends Controller
             'qty'=>'required',
         ]);
        foreach ($validated['stocks'] as $key => $stock) {
-            // 'id_booked',
-            // 'id_stock',
-            // 'jenis_layanan',
-            // 'id_customer',
-            // 'qty_booked',
-            // 'status_booked',
             $booking = new booked();
             $booking->id_stock = $stock;
+            $booking->id_booked = $stock.'-'.$validated['id_customer'].'-'.$validated['date'];
             $booking->jenis_layanan = $validated['jenis_layanan'];
             $booking->id_customer = $validated['id_customer'];
-            $booking->qty = $validated['qty_booked'];
-            $booking->qty = $validated['qty_booked'];
+            $booking->qty_booked = $validated['qty'][$key];
+            $booking->status_booked = "on_progress";
+            $booking->date = $validated['date'];
+            $booking->save();
        }
-        
-
+       return redirect('sales/sparepart/order');
     }
 }
