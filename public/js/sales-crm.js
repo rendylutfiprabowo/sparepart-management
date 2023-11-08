30130864b3b5c560c23fb69c9191241b7f799ce7
maullana19 <dedemaulana162@gmail.com>
2023-11-08 08:47:26 +0700

    Fix some feature on spareParts

--- public/js/sales-crm.js
+++ public/js/sales-crm.js
@@ -92,4 +92,54 @@ new Chart(ctxLines, {
         },
     },
 });
-// TOAST SUCCESS
+
+// Transaction Charts
+
+let myConfig = {
+    type: "bar",
+    title: {
+        text: "Data Basics",
+        fontSize: 14,
+    },
+    legend: {
+        draggable: true,
+    },
+    scaleX: {
+        // Set scale label
+        label: { text: "Days" },
+        // Convert text on scale indices
+        labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+    },
+    scaleY: {
+        // Scale label with unicode character
+        label: { text: "Sales Transaction" },
+    },
+    plot: {
+        // Animation docs here:
+        // https://www.zingchart.com/docs/tutorials/styling/animation#effect
+        animation: {
+            effect: "ANIMATION_EXPAND_BOTTOM",
+            method: "ANIMATION_STRONG_EASE_OUT",
+            sequence: "ANIMATION_BY_NODE",
+            speed: 275,
+        },
+    },
+    series: [
+        {
+            // Plot 1 values, linear data
+            values: [23, 20, 27, 29, 25, 17, 15],
+            text: "Week 1",
+        },
+        {
+            // Plot 2 values, linear data
+            values: [35, 42, 33, 49, 35, 47, 35],
+            text: "Week 2",
+        },
+    ],
+};
+
+// Render Method[3]
+zingchart.render({
+    id: "totalProfitChart",
+    data: myConfig,
+});
--- resources/views/template/new_layout.blade.php
+++ resources/views/template/new_layout.blade.php
@@ -7,50 +7,39 @@
     <meta name="description" content="">
     <title>Trafindo - @yield('title')</title>
 
-    {{-- LINKS --}}
+
     <link rel="shortcut icon" href="https://www.trafoindonesia.com/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
         integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
         crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="https://cdn.zingchart.com/zingchart.min.js"></script>
     <link href="{{ asset('/css/new-layout.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/style.css') }}" rel="stylesheet">
-    {{-- STYLE CUSTOM --}}
 
 </head>
 
 <body class="bg-secondary-subtle">
     <header class="navbar sticky-top flex-md-nowrap p-1 bg-white shadow-sm">
-        {{-- TOOGLE MOBILE --}}
+
         <button class="navbar-toggler d-md-none collapsed" type="button" data-bs-toggle="collapse"
             data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false"
             aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        {{-- LOGO --}}
+
         <div>
             <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#"> <img src="/Asset/LogoTrafoindo.png"
                     width="120" height="50" class="d-inline-block" alt="LOGO"></a>
         </div>
-
-        <div id="searchNavbar">
-            <div class="input-group">
-                <input type="text" class="form-control rounded  bg-secondary-subtle" placeholder="Search"
-                    aria-label="Search sidebars">
-                <button class="btn btn-danger" type="button"><i class="bi bi-search"></i></button>
-            </div>
-        </div>
-        {{-- NOTIFICATION --}}
         <div>
             <x-notification />
         </div>
 
-        {{-- USER MENU --}}
-        <div class="btn-group">
-            <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                {{ Auth::user()->username }}
+        {{-- <div class="btn-group">
+            <button type="button" class="btn btn-danger me-2" data-bs-toggle="dropdown" aria-expanded="false">
+                <i class="bi bi-person-fill"></i> {{ Auth::user()->username }}
             </button>
             <ul class="dropdown-menu dropdown-menu-lg-end">
                 <li><button class="dropdown-item" type="button">Profile</button></li>
@@ -62,6 +51,44 @@
                     @csrf
                 </form>
             </ul>
+        </div> --}}
+
+        <div class="btn-group">
+            <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown"
+                data-bs-display="static" aria-expanded="false">
+                {{ Auth::user()->username }}
+            </button>
+            <ul class="dropdown-menu dropdown-menu-lg-end dropdown-menu-end">
+                <li>
+                    <a class="dropdown-item d-flex gap-2 align-items-center" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-person" viewBox="0 0 16 16">
+                            <path
+                                d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z" />
+                        </svg>
+                        Profile
+                    </a>
+                </li>
+                <li>
+                    <hr class="dropdown-divider">
+                </li>
+                <li>
+                    <a class="dropdown-item dropdown-item-danger d-flex gap-2 align-items-center" href="#"
+                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z" />
+                            <path fill-rule="evenodd"
+                                d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z" />
+                        </svg>
+                        Logout
+                    </a>
+                    <form id="logout-form" action="/logout" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+            </ul>
         </div>
     </header>
 
@@ -71,23 +98,23 @@
             {{-- SIDEBARS --}}
             @extends('template.sales_sidebar')
 
-            {{-- CONTENTS --}}
-            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-3 pt-3 ">
+            {{-- WRAP CONTENTS --}}
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-3 pt-3 mb-5">
                 {{-- MAIN --}}
                 @yield('contents')
                 {{-- Footers --}}
-
             </main>
-            <div class="col-md-9 ms-sm-auto col-lg-10 px-md-1 text-bg-light p-4 mt-4">
-                <footer class="sticky-footer">
-                    <div class="container my-auto">
-                        <div class="copyright my-auto text-center">
-                            <span class="text-muted">Copyright &copy; Trafoindo 2023</span>
-                        </div>
+
+            <div class="col-md-9 ms-sm-auto col-lg-10 p-2 footer-custom bg-white">
+                <div class=" text-center">
+                    <div class="">
+                        <small class="text-secondary">&copy; 2023 PT. Trafindo, All rights reserved.</small>
                     </div>
-                </footer>
+                </div>
             </div>
         </div>
+
+
     </div>
 
     {{-- SCRIPT --}}
@@ -100,6 +127,8 @@
     </script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="{{ asset('/js/sales-crm.js') }}"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
 </body>
 
 </html>
