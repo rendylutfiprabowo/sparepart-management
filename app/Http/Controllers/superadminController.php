f7a9cc6de45b1e46d286c56f58dcb7b48ee30191
RendyLutfiPrabowo <81194811+rendylutfiprabowo@users.noreply.github.com>
2023-12-11 10:23:15 +0700

    update dashbard superadmin tss

--- app/Http/Controllers/superadminController.php
+++ app/Http/Controllers/superadminController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
-
 use App\Http\Controllers\Controller;
+use App\Models\customer;
+use App\Models\order;
+use App\Models\project;
+use App\Models\sales;
+use App\Models\sample;
 use Illuminate\Http\Request;
 
 
@@ -12,7 +16,48 @@ class superadminController extends Controller
 
     public function dashboard()
     {
-        return view('superadmin.dashboard');
+        // ================ DASHBOARD SALES CRM =======================    
+        $customersTotal = customer::count();
+        $projectsTotal = project::count();
+        $salesData = sales::all();
+        $orderSpareparts = order::all();
+        $totalOrderSP = order::count();
+        $oilSample = sample::count();
+
+        // Calculations for charts
+        $divideNumber = 1000;
+        $percentageCustomers = ($customersTotal / $divideNumber) * 100;
+        $percentageProjects = ($projectsTotal / $divideNumber) * 100;
+        $percentageSales = $percentageCustomers + $percentageProjects;
+
+        // ================ DASHBOARD OIL LAB=======================  
+        $totalDGA = Sample::whereHas('scope', function ($query) {
+            $query->where('id_scope', 220); // ID untuk DGA
+        })->count();
+        $totalFuran = Sample::whereHas('scope', function ($query) {
+            $query->where('id_scope', 842); // ID untuk Furan
+        })->count();
+        $totalOA = Sample::whereHas('scope', function ($query) {
+            $query->where('id_scope', 399); // ID untuk OA
+        })->count();
+        $totalAllSamples = $totalDGA + $totalFuran + $totalOA;
+
+        return view('superadmin.dashboard', compact(
+            'customersTotal',
+            'projectsTotal',
+            'totalOrderSP',
+            'salesData',
+            'percentageSales',
+            'percentageCustomers',
+            'percentageProjects',
+            'oilSample',
+            'totalDGA',
+            'totalFuran',
+            'totalOA',
+            'totalAllSamples'
+        ));
+
+        
     }
 
     public function createaccount()
@@ -20,24 +65,24 @@ class superadminController extends Controller
         return view('superadmin.createaccount');
     }
 
-    // public function storecreateaccount()
-    // {
-    //     $role = $request->input('role');
+    public function storecreateaccount($request)
+    {
+        $role = $request->input('role');
 
-    //     if ($role == 'admin') {
-    //         Admin::create([
-    //             'name' => $request->input('name'),
-    //             'email' => $request->input('email'),
-    //             'password' => bcrypt($request->input('password')),
-    //         ]);
-    //     } elseif ($role == 'user') {
-    //         User::create([
-    //             'name' => $request->input('name'),
-    //             'email' => $request->input('email'),
-    //             'password' => bcrypt($request->input('password')),
-    //         ]);
-    //     }
+        // if ($role == 'admin') {
+        //     AdminOilLab::create([
+        //         'name' => $request->input('name'),
+        //         'email' => $request->input('email'),
+        //         'password' => bcrypt($request->input('password')),
+        //     ]);
+        // } elseif ($role == 'user') {
+        //     Ad::create([
+        //         'name' => $request->input('name'),
+        //         'email' => $request->input('email'),
+        //         'password' => bcrypt($request->input('password')),
+        //     ]);
+        // }
 
-    //     return redirect('/login');
-    // }
+        // return redirect('/login');
+    }
 }
